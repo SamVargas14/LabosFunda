f30e1f335026238fa4d21214cbc9cf9b6d849919
SamVargas14 <00037720@uca.edu.sv>
2020-05-06 17:48:15 -0600

    Compras

---
+++ Laboratorio3/Ejercicio4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string n;
+    int c;
+    float $,p;
+    cout<<"Producto: "; cin>>n;
+    cout<<"Precio: $"; cin>>p;
+    cout<<"Cantidad comprada: "; cin>>c;
+    $ = p * c;
+    cout<<"\nTotal gastado: $"<<$;
+    return 0;}
