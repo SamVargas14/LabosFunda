4ba1e1825b54993a7a2d1429883fac781e5c8452
SamVargas14 <00037720@uca.edu.sv>
2020-05-06 14:23:02 -0600

    Formula cuadratica

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -2,12 +2,11 @@
 #include <math.h>
 using namespace std;
  int main(){
-     int a,b,c;
-     float x1,x2;
+     float a,b,c,x1,x2;
      cout<<"Ingrese valor de a: "; cin>>a;
      cout<<"Ingrese valor de b: "; cin>>b;
      cout<<"Ingrese valor de c: "; cin>>c;
-     x1 = (-b + sqrt(pow(b,2) - 4*a*c))/2*a;
-     x2 = (-b - sqrt(pow(b,2) - 4*a*c))/2*a;
+     x1 = (-b + sqrt(pow(b,2) - 4*a*c))/(2*a);
+     x2 = (-b - sqrt(pow(b,2) - 4*a*c))/(2*a);
      cout<<"x1= "<<x1<<"\nx2= "<<x2;
      return 0; }
