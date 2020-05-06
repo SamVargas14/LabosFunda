5470014fdc2de2f93aef1d5694575f5d685d4c9c
SamVargas14 <00037720@uca.edu.sv>
2020-05-06 16:31:39 -0600

    Promedios

---
+++ Laboratorio3/Ejercicio1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c;
+    float p;
+    cout<<"Ingrese valor 1: "; cin>>a;
+    cout<<"Ingrese valor 2: "; cin>>b;
+    cout<<"Ingrese valor 3: "; cin>>c;
+    p = (a + b + c) / 3;
+    cout<<"\nSupromedio es: "<<p;
+    return 0;
+}
