df8fb61b2bfa495d61e1e53fae3e4bb549d52a37
SamVargas14 <00037720@uca.edu.sv>
2020-06-27 10:11:46 -0600

    fix

--- Laboratorio6/suma.cpp
+++ Laboratorio6/suma.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
 using namespace std;
-int main(void){
+void arreglos (int [],int [],int);
+void suma (int[],int[],int[],int);
+void resultado (int[],int);
+
+int main(){
     int n;
-    cout<<"Ingresar tama\244o del arreglo:";cin>>n;
+    cout<<"Ingresar tama\244o de los arreglos: ";cin>>n;
     int a[n],b[n],c[n];
+    arreglos(a,b,n);
+    suma(a,b,c,n);
+    resultado(c,n);
+    return 0;
+}
+void arreglos(int a[],int b[], int n){
     cout<<"Elementos del arreglo A:"<<endl;
     for(int i=0;i<n;i++){
-    cout<<"Ingrese los elementos: ";cin>>a[i];
+    cin>>a[i];
     }
     cout<<"Los elementos del arreglo B:"<<endl;
     for(int i=0;i<n;i++){
-    cout<<"Elemento ["<<i<<"]:";cin>>b[i];
+    cin>>b[i];
     }
+}
+void suma(int a[],int b[],int c[], int n){
     for(int i=0;i<n;i++){
     c[i]=a[i]+b[i];
     }
-    cout<<"La suma de los arreglos:";
+}
+void resultado(int c[], int n){
+    cout<<"La suma de los arreglos A y B es:";
     for(int i=0;i<n;i++){
     cout<<" "<<c[i];
     }
